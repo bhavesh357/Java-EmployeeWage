09d871264cedfd4af45f1079826ec54e64862295
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-16 22:20:29 +0530

    MODIFIED: to use ArrayList instead of array

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -3,18 +3,16 @@ package EmployeeWage;
 import java.util.ArrayList;
 
 public class EmployeeBuilder {
-    static CompanyEmpWage[] companies;
+    static ArrayList<CompanyEmpWage> companies;
 
     public EmployeeBuilder(){
         new EmpWageBuilder(companies);
     }
     public static void main(String[] args){
         //variables
-        companies=new CompanyEmpWage[2];
-        //companies.add(new CompanyEmpWage("Reliance",20,100,20));
-        //companies.add(new CompanyEmpWage("Bridgelabz",40,80,24));
-        companies[0]=new CompanyEmpWage("Reliance",20,100,20);
-        companies[1]=new CompanyEmpWage("Bridgelabz",40,80,24);
+        companies=new ArrayList<CompanyEmpWage>();
+        companies.add(new CompanyEmpWage("Reliance",20,100,10));
+        companies.add(new CompanyEmpWage("Bridgelabz",30,80,24));
         new EmployeeBuilder();
     }
 }
