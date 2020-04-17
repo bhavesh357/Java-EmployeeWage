9579266cf17cb1faf98a2420ec3d15a2c29902be
Bhavesh Kadam <bkadam357@gmail.com>
2020-04-17 21:29:39 +0530

    REFACTORED: remove unnecessary variable

--- EmployeeBuilder.java
+++ EmployeeBuilder.java
@@ -3,17 +3,17 @@ package EmployeeWage;
 import java.util.ArrayList;
 
 public class EmployeeBuilder {
-    static ArrayList<CompanyEmpWage> companies;
 
-    public EmployeeBuilder(){
+    public EmployeeBuilder(ArrayList<CompanyEmpWage> companies){
         EmpWageBuilder builder=new EmpWageBuilder(companies);
-        builder.getTotalWage("Reliance",companies);
+        int wage=builder.getTotalWage("Reliance",companies);
+        System.out.println(wage);
     }
     public static void main(String[] args){
         //variables
-        companies=new ArrayList<CompanyEmpWage>();
+        ArrayList<CompanyEmpWage> companies=new ArrayList<CompanyEmpWage>();
         companies.add(new CompanyEmpWage("Reliance",20,100,10));
         companies.add(new CompanyEmpWage("Bridgelabz",30,80,24));
-        new EmployeeBuilder();
+        new EmployeeBuilder(companies);
     }
 }
